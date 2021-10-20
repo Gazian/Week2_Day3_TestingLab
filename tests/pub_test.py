128ee799291e00a3531ee3341961614a9e8c5d58
Sam <samuel.fitz@btinternet.com>
2021-10-20 11:57:33 +0100

    Committing Test test_pub_has_till

--- tests/pub_test.py
+++ tests/pub_test.py
@@ -3,7 +3,10 @@ from src.pub import Pub
 
 class TestPub(unittest.TestCase):
     def setUp(self):
-        self.pub = Pub("The Prancing Pony")
+        self.pub = Pub("The Prancing Pony", 1000)
     
     def test_pub_has_name(self):
         self.assertEqual("The Prancing Pony", self.pub.name)
+        
+    def test_pub_has_till(self):
+        self.assertEqual(1000, self.pub.till)
