5f8a2c197e4da5c89976c03cbca944ec7c672550
Sam <samuel.fitz@btinternet.com>
2021-10-20 14:12:42 +0100

    committing adding test_pub_can_add_drinks(self)

--- src/pub.py
+++ src/pub.py
@@ -6,3 +6,6 @@ class Pub:
         
     def pub_till_change(self, amount):
         self.till += amount
+        
+    def add_drinks_to_pub(self, drink):
+        self.drinks.append(drink)
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -1,5 +1,7 @@
 import unittest
 from src.pub import Pub
+from src.drink import Drink
+# import pdb
 
 class TestPub(unittest.TestCase):
     def setUp(self):
@@ -15,4 +17,10 @@ class TestPub(unittest.TestCase):
         self.pub.pub_till_change(4.50)
         self.assertEqual(1004.50, self.pub.till)
     
-    
+    def test_pub_can_add_drinks(self):
+        drink = Drink("Beavertown IPA", 4.50)
+        self.pub.add_drinks_to_pub(drink)
+        # pdb.set_trace()
+        # print(self.pub.drinks[0].name)
+        # print(self.pub.drinks[0].price)
+        self.assertEqual(1, len(self.pub.drinks))
